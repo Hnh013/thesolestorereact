7ee07c172124d8bbb8646cccfe4f2571f0da5aa6
Harshit <najay357@gmail.com>
2022-04-05 17:46:14 +0530

    removed commented lines

--- src/frontend/templates/pages/Wishlist.js
+++ src/frontend/templates/pages/Wishlist.js
@@ -1,33 +1,11 @@
 import React from 'react';
 import { useUser } from '../../contexts/userContext';
-import { removeItemFromWishlist , addItemToWishlist } from '../../services/wishlistService';
 import '../../styles/pages/wishlist.css';
 import { Product } from '../components';
 
 export const Wishlist = () => {
-    const { userState , userDispatcher } = useUser();
+    const { userState } = useUser();
     
-    const ATW = async (myToken , productDetails) => {
-        console.log('in wishlist');
-        const response = await addItemToWishlist(myToken , productDetails);
-        console.log(response);
-        // userDispatcher({ 
-        //     type: 'addItemToWishlist' , 
-        //     payload : { ...userState.foundUser , wishlist : response.actionResponse.data.wishlist}   
-        // });
-        
-    }
-
-    const RFW = async (myToken, productDetails) => {
-        const response = await removeItemFromWishlist(myToken , productDetails._id);
-        console.log(response);
-        // userDispatcher({ 
-        //     type: 'removeItemFromWishlist' , 
-        //     payload : { ...userState.foundUser , wishlist : response.actionResponse.data.wishlist }   
-        // });
-        
-    }
-
     return (
         <main className="main px-xs py-xs d-flex fd-col gap-1">
             <div className="px-sm d-flex jc-c ai-c">
@@ -37,7 +15,7 @@ export const Wishlist = () => {
             </div>
             <div className="d-flex fw-wrap jc-c gap-1 pb-md">
                 { userState.foundUser &&                
-                 <> { userState.foundUser.wishlist.map( (product,index) => <Product product={product} key={index} RFW={RFW} ATW={ATW} />)} </>
+                 <> { userState.foundUser.wishlist.map( (product,index) => <Product product={product} key={index} />)} </>
                 }
             </div>
         </main>
