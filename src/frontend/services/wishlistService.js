f2fadcdffd641095365d1cf98e6cd3cb52b09236
Harshit <najay357@gmail.com>
2022-04-05 18:42:22 +0530

    used try catch in api calls

--- src/frontend/services/wishlistService.js
+++ src/frontend/services/wishlistService.js
@@ -3,40 +3,40 @@ import axios from 'axios';
 const getWishlist = async (myToken) => {
     let actionSuccess = false;
     let actionResponse;
-    await axios.get('/api/user/wishlist', { headers: { authorization: myToken }}).then((response) => {
-      actionResponse = response;
-      actionSuccess = true;
-    }).catch((error) => {
+    try {
+        actionResponse = await axios.get('/api/user/wishlist', { headers: { authorization: myToken }});
+        actionSuccess = true;
+    } catch(error) {
         actionResponse = error;
         actionSuccess = false;
-    });
-    return {actionResponse , actionSuccess};
+    }
+  return {actionResponse , actionSuccess};
 }
 
 const addItemToWishlist = async (myToken, productDetails) => {
   let actionSuccess = false;
   let actionResponse;
-  await axios.post('/api/user/wishlist', { product : productDetails } , { headers: { authorization: myToken }}).then((response) => {
-    actionResponse = response;
-    actionSuccess = true;
-  }).catch((error) => {
+  try {
+      actionResponse = await axios.post('/api/user/wishlist', { product : productDetails } , { headers: { authorization: myToken }});
+      actionSuccess = true;
+  } catch(error) {
       actionResponse = error;
       actionSuccess = false;
-  });
+  };
   return {actionResponse , actionSuccess};
 }
 
 const removeItemFromWishlist = async (myToken, productId) => {
-    let actionSuccess = false;
-    let actionResponse;
-    await axios.delete(`/api/user/wishlist/${productId}`, { headers: { authorization: myToken }}).then((response) => {
-      actionResponse = response;
+  let actionSuccess = false;
+  let actionResponse;
+  try { 
+      actionResponse = await axios.delete(`/api/user/wishlist/${productId}`, { headers: { authorization: myToken }});
       actionSuccess = true;
-    }).catch((error) => {
-        actionResponse = error;
-        actionSuccess = false;
-    });
-    return {actionResponse , actionSuccess};
-  }
+  } catch(error) {
+      actionResponse = error;
+      actionSuccess = false;
+  };
+  return {actionResponse , actionSuccess};
+}
 
 export { addItemToWishlist , removeItemFromWishlist , getWishlist };
